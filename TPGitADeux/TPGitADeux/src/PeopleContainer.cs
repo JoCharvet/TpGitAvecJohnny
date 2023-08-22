750e31a74c9659122a0aef533b8745f23fc34450
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-08-22 15:49:02 +0200

    Create PeopleContainer

---
+++ TPGitADeux/TPGitADeux/src/PeopleContainer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPGitADeux.src
+{
+    public class PeopleContainer : IPersonContainer<Person>
+    {
+        public List<Person> people { get; private set; }
+        public PeopleContainer(List<Person> _people = null) 
+        {
+            if (_people == null)
+            {
+                people = new List<Person>();
+            }
+            else
+            {
+                people= _people;
+            }
+        }
+        public List<Person>? SortByFirstName()
+        {
+            if (people.Count > 0)
+            {
+                Person[] ap = people.OrderBy(p => p.firstName).ToArray();
+                List<Person> lp = new List<Person>();
+                foreach (Person p in ap)
+                {
+                    lp.Add(p);
+                }
+                return lp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Person>? SortByLastName()
+        {
+            if (people.Count > 0)
+            {
+                Person[] ap = people.OrderBy(p => p.lastName).ToArray();
+                List<Person> lp = new List<Person>();
+                foreach (Person p in ap)
+                {
+                    lp.Add(p);
+                }
+                return lp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
